ae594b203a9558f04071f12aa8916c4a824ecaad
Pradeep <pg649599@gmail.com>
2025-05-05 03:21:43 +0530

    Aboutme

--- Frontend/src/assets/utils/Skills.js
+++ Frontend/src/assets/utils/Skills.js
@@ -3,14 +3,14 @@ import { FaJsSquare, FaReact, FaNodeJs, FaPython, FaGitAlt, FaDatabase } from "r
 import { SiDjango, SiTailwindcss } from "react-icons/si";
 
 const skills = [
-  { name: "JavaScript", level: 90, icon: FaJsSquare },
-  { name: "React.js", level: 85, icon: FaReact },
-  { name: "Node.js", level: 75, icon: FaNodeJs },
-  { name: "Python", level: 95, icon: FaPython },
+  { name: "Python", level: 80, icon: FaPython },
   { name: "Django", level: 80, icon: SiDjango },
-  { name: "Tailwind CSS", level: 90, icon: SiTailwindcss },
   { name: "Git", level: 88, icon: FaGitAlt },
   { name: "SQL", level: 70, icon: FaDatabase },
+  { name: "JavaScript", level: 90, icon: FaJsSquare },
+  { name: "React.js", level: 55, icon: FaReact },
+  { name: "Node.js", level: 65, icon: FaNodeJs },
+  { name: "Tailwind CSS", level: 40, icon: SiTailwindcss },
 ];
 
 export default skills;
