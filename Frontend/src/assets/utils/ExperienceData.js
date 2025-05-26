df5569d69ca766ce692c7792d114158a6d1cee5d
Pradeep <pg649599@gmail.com>
2025-05-26 17:36:02 +0530

    exp order

--- Frontend/src/assets/utils/ExperienceData.js
+++ Frontend/src/assets/utils/ExperienceData.js
@@ -2,20 +2,20 @@ import { ImOffice } from "react-icons/im";
 import { PiStudentBold } from "react-icons/pi";
 
 const experiences = [
-    {
-      jobTitle: "Software Developer",
-      company: "Cyces LLP",
-      duration: "Jan 2025 - Present",
-      description: "Developed web applications using React and Django.",
-      icon: ImOffice,
-    },
-    {
-      jobTitle: "Self Learner",
-      company: "-",
-      duration: "Colege Days",
-      description: "Worked on front-end and backend projects.",
-      icon: PiStudentBold,
-    },
+  {
+    jobTitle: "Self Learner",
+    company: "-",
+    duration: "Colege Days",
+    description: "Worked on front-end and backend projects.",
+    icon: PiStudentBold,
+  },
+  {
+    jobTitle: "Software Developer",
+    company: "Cyces LLP",
+    duration: "Jan 2025 - Present",
+    description: "Developed web applications using React and Django.",
+    icon: ImOffice,
+  },
 ];
 
 export default experiences;
